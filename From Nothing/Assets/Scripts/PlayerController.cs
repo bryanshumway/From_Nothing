f236c3d091f16ac8f8aca4e38310557224e9939e
Bryan Shumway <shumwaybryanm@gmail.com>
2020-05-01 12:25:52 -0700

    Initial Commit
    Basic character movement and collision detection. Uses scratch assets.

---
+++ From Nothing/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float moveSpeed = 3f;
+    float jumpSpeed = 1f;
+    [SerializeField] LayerMask layerMask;
+    Vector3 original;
+    Vector3 flipped;
+    Rigidbody2D rigidBody;
+    Animator playerAnimator;
+    BoxCollider2D boxCollider2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+        rigidBody = GetComponent<Rigidbody2D>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+        original = new Vector3(1,1,1);
+        flipped = new Vector3(-1,1,1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+            transform.localScale = original;
+            rigidBody.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), rigidBody.velocity.y);
+            playerAnimator.SetBool("IsWalking", true);
+        }
+        else if(Input.GetAxis("Horizontal") < 0){
+            transform.localScale = flipped;
+            rigidBody.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), rigidBody.velocity.y);
+            playerAnimator.SetBool("IsWalking", true);
+        }
+        else
+        {
+            rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
+            playerAnimator.SetBool("IsWalking", false);
+        }
+        if(Input.GetAxis("Jump")!=0 && IsGrounded())
+        {
+            rigidBody.velocity += Vector2.up*jumpSpeed;
+        }
+    }
+    private bool IsGrounded()
+    {
+        RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down
+        , .1f, layerMask);
+        return raycastHit2d.collider !=null;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # From_Nothing
  
+To make changes first make a fork for yourself. Then use github desktop to pull your fork to your computer. After the changes are made push them to your fork and make a merge request with the original.
