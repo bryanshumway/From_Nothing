a063ceeca0f619a55097d30c2445225158e47cd2
Bryan Shumway <shumwaybryanm@gmail.com>
2020-05-07 16:39:19 -0700

    Revert "Merge pull request #2 from DBriq/master"
    This reverts commit 880c8a31644f0d99339c181a2483b132c0333c32, reversing
    changes made to a9d798a08fe7e9e3bb67a7b49a99d7ca66964dd6.

--- From Nothing/Assets/Scripts/PlayerController.cs
+++ From Nothing/Assets/Scripts/PlayerController.cs
@@ -1,35 +1,55 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    //Variables
-    public float speed = 6.0F;
-    public float jumpSpeed = 8.0F;
-    public float gravity = 20.0F;
-    private Vector3 moveDirection = Vector3.zero;
+    float moveSpeed = 3f;
+    float jumpSpeed = 1f;
+    [SerializeField] LayerMask layerMask;
+    Vector3 original;
+    Vector3 flipped;
+    Rigidbody2D rigidBody;
+    Animator playerAnimator;
+    BoxCollider2D boxCollider2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+        rigidBody = GetComponent<Rigidbody2D>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+        original = new Vector3(1,1,1);
+        flipped = new Vector3(-1,1,1);
+    }
 
+    // Update is called once per frame
     void Update()
     {
-        CharacterController controller = GetComponent<CharacterController>();
-        // is the controller on the ground?
-        if (controller.isGrounded)
+        if(Input.GetAxis("Horizontal") > 0)
         {
-            //Feed moveDirection with input.
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
-            moveDirection = transform.TransformDirection(moveDirection);
-            //Multiply it by speed.
-            moveDirection *= speed;
-            //Jumping
-            if (Input.GetButton("Jump"))
-                moveDirection.y = jumpSpeed;
-
+            transform.localScale = original;
+            rigidBody.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), rigidBody.velocity.y);
+            playerAnimator.SetBool("IsWalking", true);
         }
-        //Applying gravity to the controller
-        moveDirection.y -= gravity * Time.deltaTime;
-        //Making the character move
-        controller.Move(moveDirection * Time.deltaTime);
-        transform.position = new Vector3(transform.position.x, transform.position.y, 1);
-
+        else if(Input.GetAxis("Horizontal") < 0){
+            transform.localScale = flipped;
+            rigidBody.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), rigidBody.velocity.y);
+            playerAnimator.SetBool("IsWalking", true);
+        }
+        else
+        {
+            rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
+            playerAnimator.SetBool("IsWalking", false);
+        }
+        if(Input.GetAxis("Jump")!=0 && IsGrounded())
+        {
+            rigidBody.velocity += Vector2.up*jumpSpeed;
+        }
+    }
+    private bool IsGrounded()
+    {
+        RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down
+        , .1f, layerMask);
+        return raycastHit2d.collider !=null;
     }
 }
