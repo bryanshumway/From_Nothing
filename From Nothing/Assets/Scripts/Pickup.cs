939ca16d68bec443f9c7f8613118c126f29f1d54
rlandon96 <50156134+rlandon96@users.noreply.github.com>
2020-05-24 00:03:08 -0700

    From_Nothing_01
    - Initial concept

---
+++ From Nothing/Assets/Scripts/Pickup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+
+    public GameObject pickupSpot;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            this.transform.parent = pickupSpot.transform;
+            this.transform.localPosition = new Vector3(0, 0, 0);
+            this.GetComponent<Collider2D>().enabled = false;
+        }
+    }
+
+}
--- From Nothing/Assets/Scripts/PlayerController.cs
+++ From Nothing/Assets/Scripts/PlayerController.cs
@@ -4,64 +4,62 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    float moveSpeed = 3f;
-    float jumpSpeed = 1f;
+    public float moveSpeed = 3f;
+    public float jumpSpeed = 1f;
     [SerializeField] LayerMask layerMask;
     Vector3 original;
-    Vector3 flipped;
     Rigidbody2D rigidBody;
     Animator playerAnimator;
-    BoxCollider2D boxCollider2D;
+    Collider2D boxCollider2D;
     // Start is called before the first frame update
     void Start()
     {
-        playerAnimator = GetComponent<Animator>();
+        //playerAnimator = GetComponent<Animator>();
         rigidBody = GetComponent<Rigidbody2D>();
-        boxCollider2D = GetComponent<BoxCollider2D>();
-        original = new Vector3(1,1,1);
-        flipped = new Vector3(-1,1,1);
+        boxCollider2D = GetComponent<Collider2D>();
+        original = transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetAxis("Horizontal") > 0 && IsMoveRightPossible())
+        if(Input.GetKey(KeyCode.D))
         {
             transform.localScale = original;
-            rigidBody.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), rigidBody.velocity.y);
-            playerAnimator.SetBool("IsWalking", true);
+            rigidBody.velocity = new Vector2(moveSpeed * 1, rigidBody.velocity.y);
+            //playerAnimator.SetBool("IsWalking", true);
         }
-        else if(Input.GetAxis("Horizontal") < 0 && IsMoveLeftPossible()){
-            transform.localScale = flipped;
-            rigidBody.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), rigidBody.velocity.y);
-            playerAnimator.SetBool("IsWalking", true);
+        else if(Input.GetKey(KeyCode.A)){
+            transform.localScale = new Vector3(-original.x, original.y, original.z);
+            rigidBody.velocity = new Vector2(moveSpeed * -1, rigidBody.velocity.y);
+            //playerAnimator.SetBool("IsWalking", true);
         }
         else
         {
             rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
-            playerAnimator.SetBool("IsWalking", false);
+            //playerAnimator.SetBool("IsWalking", false);
         }
-        if(Input.GetAxis("Jump")!=0 && IsGrounded())
+        if(Input.GetButtonDown("Jump") && IsGrounded())
         {
-            rigidBody.velocity += Vector2.up*jumpSpeed;
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpSpeed);
         }
     }
     private bool IsGrounded()
     {
         RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down
         , .1f, layerMask);
-        return raycastHit2d.collider !=null;
+        return raycastHit2d.collider;
     }
     private bool IsMoveRightPossible()
     {
         RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.right
         , .1f, layerMask);
-        return raycastHit2d.collider ==null;
+        return !raycastHit2d.collider;
     }
     private bool IsMoveLeftPossible()
         {
         RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.left
         , .1f, layerMask);
-        return raycastHit2d.collider ==null;
+        return !raycastHit2d.collider;
     }
 }
