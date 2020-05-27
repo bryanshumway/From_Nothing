b0947d5400f2f923e492478f9b296e949c9b662e
M@ <60533071+xblisteredavalon@users.noreply.github.com>
2020-05-26 18:37:01 -0700

    FMOD Integration
    - integrated FMOD plugin
    - replaced Unity Audio Listeners with FMOD Studio Listeners
    - added background music play event to MainCamera game object
    - added footstep sound play event to PlayerController script
    - added FMODMixer script to control Music and SFX buses, still needs to be hooked up to Settings menu sliders (can use sliders within inspector view for now)

---
+++ From Nothing/Assets/Scripts/FMODMixer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FMODMixer : MonoBehaviour
+{
+
+    FMOD.Studio.Bus musicBus;
+    FMOD.Studio.Bus sfxBus;
+
+    [SerializeField]
+    [Range(-80f, 10f)]
+    private float musicBusVolume;
+    private float musicVolume;
+
+    [SerializeField]
+    [Range(-80f, 10f)]
+    private float sfxBusVolume;
+    private float sfxVolume;
+
+    private void Start()
+    {
+        musicBus = FMODUnity.RuntimeManager.GetBus("bus:/Music");
+        sfxBus = FMODUnity.RuntimeManager.GetBus("bus:/SFX");
+    }
+
+    private void Update()
+    {
+        musicVolume = Mathf.Pow(10.0f, musicBusVolume / 20f);
+        musicBus.setVolume(musicVolume);
+
+        sfxVolume = Mathf.Pow(10.0f, sfxBusVolume / 20f);
+        sfxBus.setVolume(sfxVolume);
+    }
+
+}
