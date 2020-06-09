356a2dd77a76599c35fe00817f7e6bfe7c719f41
rlandon96 <50156134+rlandon96@users.noreply.github.com>
2020-06-09 00:21:42 -0700

    From_Nothing_10
    - Level 2 playable

---
+++ From Nothing/Assets/Scripts/Enemy_01_movement.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy_01_movement : MonoBehaviour
+{
+
+    public float speed;
+    public float distance;
+
+    private float currPos;
+    private float newPos;
+    private float currDir;
+    private GameObject messagePanel;
+    private Text messageName;
+    private Text messageText;
+    private bool startPos = true;
+
+    private void Start()
+    {
+        currPos = transform.position.x;
+        newPos = transform.position.x + distance;
+        currDir = transform.localScale.z;
+        messagePanel = GameObject.Find("MessagePanel");
+        messageName = GameObject.Find("MessageName").GetComponent<Text>();
+        messageText = GameObject.Find("MessageText").GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        if (startPos)
+        {
+            transform.Translate(0, 0, speed * Time.deltaTime);
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, currDir);
+            if (transform.position.x >= newPos)
+            {
+                startPos = false;
+            }
+        }
+        else
+        {
+            transform.Translate(0, 0, -speed * Time.deltaTime);
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, -currDir);
+            if (transform.position.x <= currPos)
+            {
+                startPos = true;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.transform.CompareTag("Player") && PlayerController.canJump == false)
+        {
+            messageName.text = "You";
+            messageText.text = "I need to find a way over this robot...";
+            messagePanel.GetComponent<Image>().color = new Color(0, 0, 0, 0.4f);
+            StopCoroutine("MessageRemove");
+            StartCoroutine("MessageRemove");
+        }
+    }
+
+    IEnumerator MessageRemove()
+    {
+        yield return new WaitForSeconds(2);
+        messageName.text = "";
+        messageText.text = "";
+        messagePanel.GetComponent<Image>().color = new Color(0, 0, 0, 0);
+    }
+
+}
---
+++ From Nothing/Assets/Scripts/Level02Dialogue.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Level02Dialogue : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject messagePanel;
+    public Text messageName;
+    public Text messageText;
+
+    private bool dialogueActive = false;
+    private int dialogueStatus;
+
+    private void Start()
+    {
+        StartCoroutine(DialogueStart());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && dialogueActive)
+        {
+            if (dialogueStatus == 0)
+            {
+                messageName.text = "Overseer";
+                messageText.text = "Are you trying to leave?";
+                dialogueStatus = 1;
+            }
+            else if (dialogueStatus == 1)
+            {
+                messageName.text = "You";
+                messageText.text = "Tell me what's going on!";
+                dialogueStatus = 2;
+            }
+            else if (dialogueStatus == 2)
+            {
+                messageName.text = "Overseer";
+                messageText.text = "You'll never leave.";
+                dialogueStatus = 3;
+            }
+            else if (dialogueStatus == 3)
+            {
+                messageName.text = "";
+                messageText.text = "The intercom turns off.";
+                dialogueStatus = 4;
+            }
+            else if (dialogueStatus == 4)
+            {
+                messageName.text = "You";
+                messageText.text = "I'm finding a way out...";
+                dialogueStatus = 5;
+            }
+            else if (dialogueStatus == 5)
+            {
+                messageName.text = "";
+                messageText.text = "";
+                messagePanel.GetComponent<Image>().color = new Color(0, 0, 0, 0);
+                player.GetComponent<PlayerController>().enabled = true;
+                dialogueStatus = 6;
+            }
+        }
+    }
+
+    IEnumerator DialogueStart()
+    {
+        yield return new WaitForSeconds(3);
+        dialogueActive = true;
+        messageName.text = "";
+        messageText.text = "You hear an intercom turn on.";
+        messagePanel.GetComponent<Image>().color = new Color(0, 0, 0, 0.4f);
+    }
+
+
+
+}
